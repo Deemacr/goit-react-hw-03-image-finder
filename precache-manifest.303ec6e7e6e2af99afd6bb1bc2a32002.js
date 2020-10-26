03733be2f805cb56f00700ac8265d50355d51714
Viktor Kirichok <viktor.kirichok@gmail.com>
2020-10-26 11:07:12 +0200

    Updates

---
+++ precache-manifest.303ec6e7e6e2af99afd6bb1bc2a32002.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f06f66db3a45452b0cc0edc12443e0d6",
+    "url": "/Deemacr/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "af79b1aa5f4ab75307e2",
+    "url": "/Deemacr/goit-react-hw-03-image-finder/static/css/main.9105a4ac.chunk.css"
+  },
+  {
+    "revision": "0d46cadff7418424028a",
+    "url": "/Deemacr/goit-react-hw-03-image-finder/static/js/2.95cdcd77.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/Deemacr/goit-react-hw-03-image-finder/static/js/2.95cdcd77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af79b1aa5f4ab75307e2",
+    "url": "/Deemacr/goit-react-hw-03-image-finder/static/js/main.591fdd37.chunk.js"
+  },
+  {
+    "revision": "5760e7524c6ffc138976",
+    "url": "/Deemacr/goit-react-hw-03-image-finder/static/js/runtime-main.9a5069ce.js"
+  }
+]);
