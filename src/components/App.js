1b98ca0701892454183ca95389fd0caf23d444f9
Viktor Kirichok <viktor.kirichok@gmail.com>
2020-10-26 11:05:23 +0200

    Hw 3 added

---
+++ src/components/App.js
@@ -0,0 +1,99 @@
+import React, { Component } from "react";
+import SearchBar from "./SearchBar";
+import PicturesApi from "../tools/PicturesApi";
+import ImageGallery from "./ImageGallery";
+import Button from "./Button";
+import Loader from "./Loader";
+import Modal from "./Modal";
+import styled from "styled-components";
+
+const AppStyled = styled.div`
+	display: grid;
+	grid-template-columns: 1fr;
+	grid-gap: 16px;
+	padding-bottom: 24px;
+`;
+
+export default class App extends Component {
+	state = {
+		pictures: [],
+		id: "",
+		isLoading: false,
+		largeImageUrl: "",
+		showModal: false,
+		page: 1,
+		searchQuery: "",
+		error: false,
+	};
+
+	componentDidMount() {}
+
+	componentDidUpdate(prevProps, prevState) {
+		console.log("Rerendered: ", Date.now());
+		const prevQuery = prevState.searchQuery;
+		const nextQuery = this.state.searchQuery;
+		if (prevQuery !== nextQuery) {
+			this.fetchPictures();
+		}
+		if (this.state.page > 2) {
+			window.scrollTo({
+				top: document.documentElement.scrollHeight,
+				behavior: "smooth",
+			});
+		}
+	}
+
+	fetchPictures = () => {
+		const { searchQuery, page } = this.state;
+		this.setState({ isLoading: true });
+		PicturesApi.fetchPicturesWithQuery(searchQuery, page)
+			.then((pictures) =>
+				this.setState((prevState) => ({
+					pictures: [...prevState.pictures, ...pictures],
+					page: prevState.page + 1,
+				}))
+			)
+			.catch((error) => this.setState({ error: true }))
+			.finally(() => this.setState({ isLoading: false }));
+	};
+
+	handleSearchFormSubmit = (query) => {
+		this.setState({
+			searchQuery: query,
+			page: 1,
+			pictures: [],
+		});
+	};
+
+	toggleModalImg = (largeImageUrl) => {
+		this.setState({ largeImageUrl: largeImageUrl });
+		this.setState({ showModal: !this.state.showModal });
+	};
+
+	handleModalImg = () => {};
+
+	render() {
+		const { pictures, isLoading, error, showModal, largeImageUrl } = this.state;
+
+		return (
+			<AppStyled>
+				{error && <p>Whoops, something went wrong</p>}
+				<SearchBar onSubmit={this.handleSearchFormSubmit}></SearchBar>
+				{pictures.length > 0 && (
+					<ImageGallery
+						pictures={pictures}
+						toggleImg={this.toggleModalImg}
+						onClose={this.toggleModalImg}
+					></ImageGallery>
+				)}
+				{showModal && (
+					<Modal onClose={this.toggleModalImg} largeImageUrl={largeImageUrl} />
+				)}
+				{pictures.length > 0 && !isLoading && (
+					<Button type="button" onClick={this.fetchPictures}></Button>
+				)}
+				{isLoading && <Loader />}
+			</AppStyled>
+		);
+	}
+}
---
+++ src/components/ImageGallery.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import ImageGalleryItem from "./ImageGalleryItem";
+
+import styled from "styled-components";
+
+const StyledImageGallery = styled.ul`
+	display: grid;
+	max-width: calc(100vw - 48px);
+	grid-template-columns: repeat(auto-fill, minmax(320px, 1fr));
+	grid-gap: 16px;
+	margin-top: 0;
+	margin-bottom: 0;
+	padding: 0;
+	list-style: none;
+	margin-left: auto;
+	margin-right: auto;
+`;
+
+export default class ImageGallery extends Component {
+	handleClick = (largeImageURL) => {
+		this.props.onClose(largeImageURL);
+	};
+
+	render() {
+		const { pictures } = this.props;
+
+		return (
+			<StyledImageGallery>
+				{pictures.map(({ id, webformatURL, largeImageURL }) => (
+					<ImageGalleryItem
+						key={id}
+						id={id}
+						imageUrl={webformatURL}
+						onClick={() => this.handleClick(largeImageURL)}
+					/>
+				))}
+			</StyledImageGallery>
+		);
+	}
+}
