e1a7c7f34320280d5c85d89fcb63fdb3158e3474
Viktor Kirichok <viktor.kirichok@gmail.com>
2020-10-26 13:41:49 +0200

    Refurbished

--- src/tools/PicturesApi.js
+++ src/tools/PicturesApi.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 const fetchPicturesWithQuery = (searchQuery, page = 1) => {
-	// const key = "18847688-8d11b7eecf2ca80c5f8524db0";
 	return axios
 		.get(
 			`https://pixabay.com/api/?key=18847688-8d11b7eecf2ca80c5f8524db0&q=${searchQuery}&image_type=photo&per_page=12&page=${page}`
